8cadfbc831aa296503cdf365e25f42a01501ab15
ZhukoMuh <golosenko95@gmail.com>
2023-01-21 10:42:10 +0200

    fix class str decorator. add closure

--- homework_14_01_2023/class_str_in_file.py
+++ homework_14_01_2023/class_str_in_file.py
@@ -1,15 +1,24 @@
 def save_str_decorator(cls):
     """write result of cls.__str__ to {cls.__name__}.txt file"""
-    file_name = cls.__name__
-    with open(f'{file_name}.txt', 'w') as file:
-        file.write(f'{cls.__str__(cls)}')
 
-    return cls
+    def inner(*args, **kwargs):
+        file_name = cls.__name__
+        cls.__init__(cls, *args, **kwargs)
+        with open(f'{file_name}.txt', 'w') as file:
+            file.write(f'{cls.__str__(cls)}')
+
+        return cls
+
+    return inner
 
 
 @save_str_decorator
 class Dog:
+    def __init__(self):
+        self.color = 'Black'
+
     def __str__(self):
         return f'Woof-woof'
 
 
+pug = Dog()
