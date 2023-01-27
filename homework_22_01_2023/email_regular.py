ac03ad57180a02ce3c4b75280b66804ad6a49818
ZhukoMuh <golosenko95@gmail.com>
2023-01-27 14:32:33 +0200

    complete 22.01.2023 homework

---
+++ homework_22_01_2023/email_regular.py
@@ -0,0 +1,25 @@
+import re
+
+
+def email_validate(email: str):
+    """validate email with regular pattern"""
+    email_pattern = r'[a-zA-Z0-9]*(-?\.?[_a-zA-Z0-9])*@[a-zA-Z0-9]+\.+[a-z]+'
+    if re.match(email_pattern, email):
+        return email
+    return 'Email is not validate'
+
+
+emails = ['johnsmith@example.com',
+          'yourname@yourcompany.com',
+          'info@webdomain.com',
+          'firstname.lastname@provider.com',
+          'firstinitial.lastname@domain.tld',
+          'username@hostname.net',
+          'customername@isp.net',
+          'me@myisp.com',
+          'customer.support@company.com',
+          'admin@mydomain.org']
+
+with open('test.txt', 'a') as f:
+    for text in emails:
+        f.writelines(email_validate(text) + '\n')
